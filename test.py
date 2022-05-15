928749bbc1cecf77e7d04f546afb3a37277cc5fa
quangtrunguyen <e11778288@student.tuwien.ac.at>
2022-05-15 23:59:06 +0200

    Change test

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 from sine_frequency_calculator import sinusoidal_PWM_frequency
 from adc_calc import adc_frequency_calc
 
-def test():
+def test_sinusoidal_PWM_frequency():
     assert sinusoidal_PWM_frequency(0,1,1,1) == 0
+    
+
+def test_adc_frequency_calc():
     assert adc_frequency_calc(0,1) == 0
     
     
